baada7375f68f32ae6ed6599f319034fcc45cf0e
Victor MRosa <victor.lmr@hotmail.com>
2022-03-19 15:59:13 -0300

    Métodos implementados/finalizados

--- tadDoublyLinkedList/doublyLinkedList.py
+++ tadDoublyLinkedList/doublyLinkedList.py
@@ -13,35 +13,63 @@ class DoublyLinkedListIterator:
             self.size = 0
 
     def first_Node(self):
+        '''
+        Put the iterator under the first node
+        '''
         self.iterator = self.firstNode
 
     def last_Node(self):
+        '''
+        Put the iterator under the last node
+        '''
         self.iterator = self.lastNode
 
     def nextNode(self):
+        '''
+        Advances the iterator to the next node
+        '''
         if self.iterator:
             self.iterator = self.iterator.nextNode
 
     def undefinedIterator(self):
+        '''
+        Checks if iterator is undefined
+        '''
         if self.iterator == None:
             return True
         else:
             return False
     
     def printNode(self):
+        '''
+        Displays list items
+        '''
+        print(f'[ ', end='')
         currentNode = self.firstNode
         while currentNode:
-            print(f'{currentNode.data}')
+            print(f'{currentNode.data}', end=' ')
             currentNode = currentNode.nextNode
+        print(f']')
 
     def addNode(self, data):
+        '''
+        Insert a node after the iterator and goes under this node
+        :param data: int, string...
+        :return True or False
+        '''
         newNode = ListNode(data, None, None)
         if self.size == 0:
+            '''
+            Check if it is the first node in the list
+            '''
             self.firstNode = newNode
             self.lastNode = newNode
             self.iterator = newNode
 
         elif self.iterator == self.lastNode:
+            '''
+            Check if iterator is under last node
+            '''
             newNode.nextNode = None
             newNode.antNode = self.iterator
             self.iterator.nextNode = newNode
@@ -49,6 +77,9 @@ class DoublyLinkedListIterator:
             self.iterator = newNode
 
         else:
+            '''
+            Iterator is under an innermost element
+            '''
             newNode.antNode = self.iterator
             newNode.nextNode = self.iterator.nextNode
             self.iterator.nextNode = newNode
@@ -58,44 +89,97 @@ class DoublyLinkedListIterator:
         return True      
 
     def insNode(self, data):
+        '''
+        Insert a node before the iterator and places the iterator over it
+        :param data: int, string...
+        :return True or False
+        '''
         newNode = ListNode(data, None, None)
         if self.size == 0:
+            '''
+            Check if it is the first node in the list
+            '''
             self.firstNode = newNode
             self.lastNode = newNode
             self.iterator = newNode
         
         elif self.iterator ==  self.firstNode:
+            '''
+            Checks if the interactor is under the first node
+            '''
             newNode.nextNode = self.iterator
             self.iterator.antNode = newNode
+            self.firstNode = newNode
             self.iterator = newNode
         
         else:
+            '''
+            Iterator is under an innermost element
+            '''
             newNode.nextNode = self.iterator
             newNode.antNode = self.iterator.antNode
             self.iterator.antNode.nextNode = newNode
             self.iterator.antNode = newNode
             self.iterator = newNode
 
+        self.size += 1
+        return True
+
     def elimNode(self):
+        '''
+        Drop the node and place the iterator under the next node
+        :param void
+        :return True or False
+        '''
         if self.iterator == self.lastNode:
+            '''
+            Checks if iterator is under last node
+            '''
             if self.firstNode == self.lastNode:
+                '''
+                Check if iterator is under last node
+                '''
                 self.firstNode = None
                 self.lastNode = None
                 self.iterator = None
             else:
+                '''
+                Iterator is under last node, but has many elements
+                '''
                 self.iterator.antNode.nextNode = None
+                self.lastNode = self.iterator.antNode
                 self.iterator = self.iterator.antNode
+
+        elif self.iterator == self.firstNode:
+            '''
+            Check if iterator is under first node
+            '''
+            self.iterator.nextNode.antNode = None
+            self.firstNode = self.iterator.nextNode
+            self.iterator = self.iterator.nextNode
+            
         else:
-            if self.iterator == self.lastNode:
-                self.iterator.antNode.nextNode = None
-                self.iterator = self.iterator.antNode
-            else:
-                self.iterator.antNode.nextNode = self.iterator.nextNode
-                self.iterator.nextNode.antNode = self.iterator
-                self.iterator = self.iterator.nextNode
+            '''
+            Iterator is under an innermost element
+            '''
+            self.iterator.antNode.nextNode = self.iterator.nextNode
+            self.iterator.nextNode.antNode = self.iterator
+            self.iterator = self.iterator.nextNode
+        
+        self.size -= 1
+        return True
 
     def posNode(self, position):
+        '''
+        Put the iterator under the position received in parameter
+        :param position: int
+        :return True or False
+        '''
         if position >= 1 and position <= self.size:
+            '''
+            Check if parameter position is greater than or equal to 1 and position is less than or
+            equal to size.
+            '''
             i = 1
             self.iterator = self.firstNode
             while i < position:
--- tadDoublyLinkedList/main.py
+++ tadDoublyLinkedList/main.py
@@ -5,15 +5,35 @@ if __name__ == '__main__':
 
     list = DoublyLinkedListIterator()
 
-    '''Adicionando Valores!'''
+    '''
+    Adding multiple values - addNode()
+    '''
     list.addNode(10)
     list.addNode(20)
     list.addNode(30)
+    print(f'method addNode(): ', end='')
+    list.printNode() # [ 10 20 30 ]
+
+    '''
+    Adding multiple values - insNode()
+    '''
     list.insNode(40)
-    print(f'Adicionando Valores')
+    list.insNode(50)
+    list.insNode(60)
+    print(f'method insNode(): ', end='')
+    list.printNode() # [ 60 50 40 ]
+
+    '''
+    Operation in the TAD List
+    '''    
+    print(f'process method elimNode(): ', end='')
+    list.elimNode() 
     list.printNode()
 
-    print('Depois da tratativa')
-    list.posNode(1)
+    print(f'process method posNode(): ', end='')
+    list.posNode(2)
+    print(f'move position at data = {list.iterator.data}')
+
+    print(f'process method elimNode(): ', end='')
     list.elimNode()
     list.printNode()
