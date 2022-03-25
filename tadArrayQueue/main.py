29d0de0ee5ca3ae705995e76f4ca60937aae717a
Victor MRosa <victor.lmr@hotmail.com>
2022-03-24 22:37:40 -0300

    Atualizando arquivos

--- tadArrayQueue/main.py
+++ tadArrayQueue/main.py
@@ -3,103 +3,103 @@ from arrayQueue import ArrayQueue
 if __name__ == "__main__":
   
     def printQueue(lst):
-        copia = ArrayQueue()
-        print('[', end="")
-        while(not lst.is_empty()):
-          print(lst.first(), end=" ")
-          copia.enqueue(lst.dequeue())
-        while(not copia.is_empty()):
-          lst.enqueue(copia.dequeue())  
-        copia = None
-        print(']\n')
+      copia = ArrayQueue()
+      print('[', end="")
+      while(not lst.is_empty()):
+        print(lst.first(), end=" ")
+        copia.enqueue(lst.dequeue())
+      while(not copia.is_empty()):
+        lst.enqueue(copia.dequeue())  
+      copia = None
+      print(']\n')
 
     def printArray(lst):
-        lst.first_Node()
-        while not lst.isUndefinedIterator():
-          print(lst.get_iterator().getData(), end=" ")
-          lst.nextNode()
-        print('\n')
+      lst.first_Node()
+      while not lst.isUndefinedIterator():
+        print(lst.get_iterator().getData(), end=" ")
+        lst.nextNode()
+      print('\n')
 
     """01 Insere o elemento na fila dada uma posição"""
     def insertElementInPosition(lst, element, position):
-        copy = ArrayQueue()
-        aux = 0
-        if len(lst) == 0 and position == 0:
-          lst.enqueue(element)
-        elif (position > 0 and position < len(lst) + 1):
-          while (not array.is_empty()):
-            if aux == position: 
-              copy.enqueue(element)
-              aux += 1
-            else:
-              copy.enqueue(lst.dequeue)
-              aux +=1
-          while (not copy.is_empty()):
-            lst.enqueue(copy.dequeue())       
-        else:
-          print('Invalid Position!')
+      copy = ArrayQueue()
+      aux = 0
+      if len(lst) == 0 and position == 0:
+        lst.enqueue(element)
+      elif (position > 0 and position < len(lst) + 1):
+        while (not lst.is_empty()):
+          if aux == position: 
+            copy.enqueue(element)
+            aux += 1
+          else:
+            copy.enqueue(lst.dequeue)
+            aux +=1
+        while (not copy.is_empty()):
+          lst.enqueue(copy.dequeue())       
+      else:
+        print('Invalid Position!')
 
     """02 Concatenar filas"""
     def concatQueue(lst1, lst2):
-        copia = ArrayQueue
-        copia = ArrayQueue(lst2)
-        while (not lst2.is_empty()):
-          lst1.enqueue(lst2.dequeue())
-        while (not copia.is_empty()):
-          lst2.enqueue(copia.dequeue())
-        copia = None
+      copia = ArrayQueue
+      copia = ArrayQueue(lst2)
+      while (not lst2.is_empty()):
+        lst1.enqueue(lst2.dequeue())
+      while (not copia.is_empty()):
+        lst2.enqueue(copia.dequeue())
+      copia = None
 
     """03 Posiciona o elemento na primeira posição da fila"""
     def firstQueue(array, element):
-        copia = ArrayQueue()
-        copia.enqueue(element)
-        
-        while (not array.is_empty):
-          copia.enqueue(array.dequeue())
-            
-        while (not copia.is_empty):
-          array.enqueue(copia.dequeue())
-        copia = None
+      copia = ArrayQueue()
+      copia.enqueue(element)
+      
+      while (not array.is_empty):
+        copia.enqueue(array.dequeue())
+          
+      while (not copia.is_empty):
+        array.enqueue(copia.dequeue())
+      copia = None
 
     """04 Verifica se o elemento existe na fila"""
     def searchElement(array, element):
-        if array.is_empty:
-          return False
-        else:
-          while (not array.is_empty):
-            if (array.dequeue() == element):
-              return True
-          return False
+      if array.is_empty:
+        return False
+      else:
+        while (not array.is_empty):
+          if (array.dequeue() == element):
+            return True
+        return False
 
     def adicFilaPos(fila, elem, pos):
-        if pos > 0 and pos <= len(fila):
-            filaAux = ArrayQueue()
-            cont = 1
-            
-            while (not fila.is_empty()):
-                if cont == pos:
-                    filaAux.enqueue(elem)
-                    cont += 1
-                else:
-                    filaAux.enqueue(fila.dequeue())
-                    cont += 1
-                        
-            while (not filaAux.is_empty()):
-                fila.enqueue(filaAux.dequeue())
-        else:
-            print(f'Posição inválida')
+      if pos > 0 and pos <= len(fila):
+          filaAux = ArrayQueue()
+          cont = 1
+          
+          while (not fila.is_empty()):
+              if cont == pos:
+                  filaAux.enqueue(elem)
+                  cont += 1
+              else:
+                  filaAux.enqueue(fila.dequeue())
+                  cont += 1
+                      
+          while (not filaAux.is_empty()):
+              fila.enqueue(filaAux.dequeue())
+      else:
+          print(f'Posição inválida')
 
     def elimElemento(fila, elem):
-        filaApoio = ArrayQueue()
-        
-        while not fila.is_empty():
-            data = fila.dequeue()
-            if data != elem:
-                filaApoio.enqueue(data)
-                
-        while not filaApoio.is_empty():
-            fila.enqueue(filaApoio.dequeue())
-        return True
+      filaApoio = ArrayQueue()
+      
+      while not fila.is_empty():
+          data = fila.dequeue()
+          if data != elem:
+              filaApoio.enqueue(data)
+              
+      while not filaApoio.is_empty():
+          fila.enqueue(filaApoio.dequeue())
+      return True
     
 
     queue = ArrayQueue()
@@ -107,7 +107,5 @@ if __name__ == "__main__":
     queue.enqueue(20)
     queue.enqueue(30)
     queue.enqueue(40)
-    elimElemento(queue, 40)
-    adicFilaPos(queue, 50, 3)
-
+    queue.dequeue()
     printQueue(queue)
--- tadArrayStack/main.py
+++ tadArrayStack/main.py
@@ -1,47 +1,84 @@
 
-from tadSimplyLinkedList import SimplyLinkedList
+from tadArrayQueue.arrayQueue import ArrayQueue
+from tadSimplyLinkedList.simplyLinkedList import SimplyLinkedList
 from arrayStack import ArrayStack
 
 if __name__ == '__main__':
 
-  def printStack(s):
+  def printStack(stack):
     copia = ArrayStack()
-    
     print('[', end="")
-    while (not s.is_empty()):
-      print(s.top(), end=" ")
-      copia.push(s.pop())
+
+    while (not stack.is_empty()):
+      print(stack.top(), end=" ")
+      copia.push(stack.pop())
     
     while (not copia.is_empty()):
-        s.push(copia.pop())
+        stack.push(copia.pop())
     print(']\n')
     copia = None
 
-  def copyStackArray(array):
+  def copyStackArray(stack):
     copia = ArrayStack()
     apoio = ArrayStack()
 
-    while (not array.is_empty()):
-      apoio.push(array.pop())
+    while (not stack.is_empty()):
+      apoio.push(stack.pop())
 
     while (not apoio.is_empty()):
       copia.push(apoio.pop())
-      array.push(apoio.pop())
+      stack.push(apoio.pop())
 
     apoio = None
     return copia
 
-  def copyStackArrayWithSimplyLinkedList(array):
+  def copyStackArrayWithSimplyLinkedList(stack):
     copia = ArrayStack()
     apoio = SimplyLinkedList()
 
-    while (not array.is_empty()):
-      apoio.insertNode(array.pop())
+    while (not stack.is_empty()):
+      apoio.insertNode(stack.pop())
 
     while (not apoio.isUndefinedIterator()):
-      array.push(apoio.data)
+      stack.push(apoio.data)
       copia.push(apoio.data)
       apoio.removeNode()
 
     apoio = None
     return copia
+
+  def verifyPalindromeStack(stack):
+    copyQueue = ArrayQueue()
+    copyStack = ArrayStack()
+
+    while not stack.is_empty():
+      varStack = stack.pop()
+      copyQueue.enqueue(varStack)
+      copyStack.push(varStack)
+
+    while not stack.is_empty():
+      if copyQueue.dequeue() != copyStack.pop():
+        print(f'Não é um palindromo')
+        return False
+    
+    print(f'É um palindromo')
+    return True
+
+  def removeAllOcorrencyElementStack(stack, elem):
+    copyStack = ArrayStack()
+
+    while not stack.is_empty():
+      varStack = stack.pop()
+      if varStack != elem:
+        copyStack.push(varStack)
+
+    while not copyStack.is_empty():
+      stack.push(stack.pop())
+
+    return True
+
+  stack = ArrayStack()
+  stack.push(10)
+  stack.push(20)
+  stack.push(10)
+  printStack(stack)
